363c87239e52961ee5c87c959cabfab67daf69e5
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-16 14:05:48 -0400

    old commit to boiler folder
    submodule not uploading

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,6 @@ module.exports = {
             },
         },
         "gatsby-plugin-mdx",
-        "gatsby-plugin-themeui",
         "gatsby-plugin-sharp",
         "gatsby-transformer-sharp",
         {
