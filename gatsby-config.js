6339da88005707ee70fd8a842e1c56e951d06b50
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-16 06:14:12 -0400

    Hom, About, and Download pages added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,67 +1,62 @@
 module.exports = {
-        siteMetadata: {
-            siteUrl: "https://www.yourdomain.tld",
-            title: "musicianblog",
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "musicianblog",
+    },
+    plugins: [{
+            resolve: "gatsby-source-sanity",
+            options: {
+                projectId: "musicianblog",
+                dataset: "",
+            },
         },
-        plugins: [{
-                resolve: "gatsby-source-sanity",
-                options: {
-                    projectId: "musicianblog",
-                    dataset: "",
-                },
+        "gatsby-plugin-sass",
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
             },
-            "gatsby-plugin-sass",
-            "gatsby-plugin-gatsby-cloud",
-            "gatsby-plugin-react-helmet",
-            "gatsby-plugin-sitemap",
-            {
-                resolve: "gatsby-plugin-manifest",
-                options: {
-                    icon: "src/images/icon.png",
-                },
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/",
             },
-            "gatsby-plugin-mdx",
-            "gatsby-plugin-sharp",
-            "gatsby-transformer-sharp",
-            {
-                resolve: "gatsby-source-filesystem",
-                options: {
-                    name: "images",
-                    path: "./src/",
-                },
-                __key: "images",
+            __key: "images",
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
             },
-            {
-                resolve: "gatsby-source-filesystem",
-                options: {
-                    name: "pages",
-                    path: "./src/pages/",
-                },
-                __key: "pages",
-            },
-            `gatsby-transformer-yaml`,
-            {
-                resolve: `gatsby-source-filesystem`,
-                options: {
-                    path: `./src/data/`, // location of yaml files
-                },
+            __key: "pages",
+        },
 
-                //   Add configuration themes here
-                module.exports = {
-                    plugins: [{
-                            resolve: `gatsby-theme-notes`,
-                            options: {
-                                mdx: true,
-                                basePath: `/notes`,
-                            },
-                        },
-                        // with gatsby-plugin-theme-ui, the last theme in the config
-                        // will override the theme-ui context from other themes
-                        { resolve: `gatsby-theme-blog` },
-                    ],
-                    siteMetadata: {
-                        title: `Shadowed Site Title`,
+
+        //   Add configuration themes here
+        exports = {
+            plugins: [{
+                    resolve: `gatsby-theme-notes`,
+                    options: {
+                        mdx: true,
+                        basePath: `/notes`,
                     },
-                }
+                },
+                // with gatsby-plugin-theme-ui, the last theme in the config
+                // will override the theme-ui context from other themes
+                { resolve: `gatsby-theme-blog` },
             ],
-        };
+            siteMetadata: {
+                title: `Shadowed Site Title`,
+            },
+        }
+    ],
+};
