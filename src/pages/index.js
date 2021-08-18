b40d0dd1f4a5ff00d976ef64aa176bfe43d046b0
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-18 01:50:45 -0400

    main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,21 @@
 // Step 1: Import React
 import * as React from 'react'
-import Layout from './boiler/src/components/layout'
+import Layout from '../components/layout'
 
-{
+
+
     // Step 2: Define your component
-    const IndexPage = () => {
-        return ( <
-                Layout pageTitle = "Home Page" >
-                <
-                p > I 'm making this by following the Gatsby Tutorial.</p> < /
-                Layout >
-            ),
-            return ( <
-                main >
-                <
-                title > Home Page < /title> <
-                h1 > Welcome to my Gatsby site! < /h1> <
-                p > I 'm making this by following the Gatsby Tutorial.</p> <
-                /main>
-            ),
-    }
+    const HomePage = () => {
+        return (
+          <Layout pageTitle="HomePage" pageHeading='Heading'>
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+          </Layout>
+        )
+      }
+      
 
-}
+
 
 // Step 3: Export your component
-export default IndexPage
+
+export default HomePage
