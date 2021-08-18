b533e1a23cc44b789063eb10371f28bbb38020d8
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-18 02:51:15 -0400

    main

--- src/components/videos.js
+++ src/components/videos.js
@@ -1,11 +1,20 @@
-<main>
-    <div className={container}>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div className={container3}>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div className={container4}>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-</main>
+import * as React from 'react'
+
+
+const VideosPage = () => {
+    return (
+            <main>
+                <div className={container}>
+                <iframe width="auto" height="auto" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className={container3}>
+                <iframe width="auto" height="auto" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div className={container4}>
+                <iframe width="auto" height="auto" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </main>
+    )
+}
+
+export default VideosPage
