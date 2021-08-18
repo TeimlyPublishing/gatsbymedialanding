35457a61dc6b9330185bc92066eb950a8bb07416
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-18 02:20:14 -0400

    main

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -8,15 +8,17 @@ import Layout from '../components/layout'
 const VideosPage = () => {
     return (
       <Layout pageTitle="Videos" PageHeading='YouTube'>
-        <div className={container2}>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <div className={container3}>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        <div className={container4}>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+        <main>
+            <div className={container2}>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className={container3}>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className={container4}>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/wr8rbaHUM6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </main>    
       </Layout>
     )
   }
