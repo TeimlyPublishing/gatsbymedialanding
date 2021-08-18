2e55430068c613c70def8f7cf1a8a405e142d80b
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-18 05:04:25 -0400

    main

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -4,9 +4,8 @@ import Layout from '../components/layout'
 const GalleryPage = () => {
   return (
     <Layout pageTitle="Video Page" pageHeading='Videos'>
-      <iframe >
-          
-      </iframe>
+      <iframe width="420" height="345" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/656837876&color=%23292929&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+
     </Layout>
   )
 }
