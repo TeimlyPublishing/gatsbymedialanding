7f827e8128f75463b22987b9c20f10441b6c8ed7
TeimlyPublishing <88227604+TeimlyPublishing@users.noreply.github.com>
2021-08-18 04:52:05 -0400

    main

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -4,7 +4,9 @@ import Layout from '../components/layout'
 const GalleryPage = () => {
   return (
     <Layout pageTitle="Video Page" pageHeading='Videos'>
-      <iframe url='https://www.youtube.com/watch?v=wr8rbaHUM6Q&list=PLCU2qJekvcN0tGUhPyscCYyVn1ee7APCA'></iframe>
+      <iframe >
+          
+      </iframe>
     </Layout>
   )
 }
